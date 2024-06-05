0007c3c2418b961190dc5170f75199bfe242c336
Kirill_Kochugin <kraingor@yandex.ru>
2024-06-05 17:48:27 +0300

    DZ6_ADDED

--- src/main/java/Java_Development_Kit/DZ6/ParadoxMontyHall.java
+++ src/main/java/Java_Development_Kit/DZ6/ParadoxMontyHall.java
@@ -5,14 +5,14 @@ public class ParadoxMontyHall{
         int winsWithSwitch = 0;
         int winsWithStay = 0;
         int gamesToPlay = input;
-        Random gen = new Random();
-        for(int plays = 0;plays < gamesToPlay;plays++ ){
+        Random random = new Random();
+        for(int plays = 0;plays < gamesToPlay; plays++ ){
             int[] doors = {0,0,0};
-            doors[gen.nextInt(3)] = 1;
-            int botChoice = gen.nextInt(3);
+            doors[random.nextInt(3)] = 1;
+            int botChoice = random.nextInt(3);
             int opened;
             do{
-                opened = gen.nextInt(3);
+                opened = random.nextInt(3);
             }while(doors[opened] == 1 || opened == botChoice);
             winsWithStay += doors[botChoice];
             winsWithSwitch += doors[3 - botChoice - opened];
